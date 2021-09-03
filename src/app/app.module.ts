086b1c2cd636d310af324412f7b8b4c2ed7e4630
leozartino <leozartino@gmail.com>
2021-09-02 21:40:19 -0300

    feat: add new imports components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
