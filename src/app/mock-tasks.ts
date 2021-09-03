dbc264a9ea81db1651dc53baf0e92d5ad50d40df
leozartino <leozartino@gmail.com>
2021-09-02 21:39:31 -0300

    feat: add task component and type of Task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { ITask } from "./components/DTO/task";
+
+export const TASKS: ITask[] = [
+  {
+  id:1,
+  text:"Study Angular",
+  day:"Set 03th at 04:00pm",
+  reminder:true
+  },
+  {
+    id:2,
+    text:"Study Software quality",
+    day:"Set 03th at 09:30am",
+    reminder:true
+  },
+  {
+    id:3,
+    text:"Work out",
+    day:"Set 03th at 12:30am",
+    reminder:false
+  },
+]
